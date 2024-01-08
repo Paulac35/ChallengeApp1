46e895645f43583e3b3fd62864d7629f0f7dfe38
Paulina Andrzejczak <paulinac35@outlook.com>
2024-01-08 16:00:56 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// " Kobieta poniżej 30 lat"
+// " Ewa , lat 33"
+// " Niepełnoletni mężczyzna "
+
+bool Woman = true;
+int age = 33;
+string name = "Ewa";
+
+if (Woman == true && age < 33)
+{
+    Console.WriteLine("Kobieta poniżej 30lat");
+
+}
+
+else if (name == "Ewa" && age == 33) 
+{
+
+    Console.WriteLine("Ewa , lat 33");
+}
+
+else if (Woman = false && age < 18) 
+
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
